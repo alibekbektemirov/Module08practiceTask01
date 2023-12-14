42bf16e2b8c0298a0cb7c5ff3e72d1369373a2ba
Izbasarn <146417085+Izbasarn@users.noreply.github.com>
2023-12-14 13:00:12 +0600

    Добавьте файлы проекта.

---
+++ Module08practiceTask01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08practiceTask01
+{
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите начальный индекс: ");
+            int lower = int.Parse(Console.ReadLine());
+            Console.Write("Введите конечный индекс: ");
+            int upper = int.Parse(Console.ReadLine());
+
+            RangeOfArray arr = new RangeOfArray(lower, upper);
+
+            // Заполняем массив
+            for (int i = lower; i <= upper; i++)
+            {
+                arr[i] = i * 10;
+            }
+
+            // Чтение значений из массива
+            for (int i = lower; i <= upper; i++)
+            {
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+
+            // Пример обработки исключения при попытке доступа за пределы диапазона
+            try
+            {
+                arr[upper + 1] = 110; // Вызовет исключение
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
+
---
+++ Module08practiceTask01/RangeOfArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08practiceTask01
+{
+    public class RangeOfArray
+    {
+        private int[] array;
+        private int lowerBound;
+        private int upperBound;
+
+        public RangeOfArray(int lowerBound, int upperBound)
+        {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+            int size = upperBound - lowerBound + 1;
+            array = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (IsIndexValid(index))
+                {
+                    return array[index - lowerBound];
+                }
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+            set
+            {
+                if (IsIndexValid(index))
+                {
+                    array[index - lowerBound] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+            }
+        }
+
+        private bool IsIndexValid(int index)
+        {
+            return index >= lowerBound && index <= upperBound;
+        }
+    }
+
+}
